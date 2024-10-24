3c1f631395c7469b84a2492c211421c75a7154c9
SssJWww <401181980@qq.com>
2024-10-24 16:34:01 +0800

    first push

--- src/main/java/com/springboot/controller/UserController.java
+++ src/main/java/com/springboot/controller/UserController.java
@@ -1,4 +1,12 @@
 package com.springboot.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/user")
 public class UserController {
+    @RequestMapping("/test")
+    public String test() {
+        return "this is allen";
+    }
 }
